e4c637e4acef2165b393070e6308825920246b54
Guilherme Mendes <gui_pmendes@hotmail.com>
2023-01-06 00:12:34 -0300

    first commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import style from "./Comment.module.css";
+
+export function Comments() {
+  return (
+    <div className={style.comment}>
+      <img src="https://avatars.githubusercontent.com/u/26482927?v=4" alt="" />
+
+      <div className={style.commentBox}>
+        <div className={style.commentContent}>
+          <header>
+            <div className={style.authorAndTime}>
+              <strong>Guilherme Mendes</strong>
+              <time
+                title="05 de janeiro de 2023"
+                dataTime="2023-01-05 10:57:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentários">
+                <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!!</p>
+        </div>
+        <footer>
+            <button>
+                <ThumbsUp />
+                Aplaudir <span>20</span>
+            </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Post.jsx
@@ -0,0 +1,55 @@
+import { Comments } from "./Comment";
+import style from "./Post.module.css";
+export function Post() {
+  return (
+    <article className={style.post}>
+      <header className={style.postHeader}>
+        <div className={style.author}>
+          <img
+            className={style.avatar}
+            src="https://avatars.githubusercontent.com/u/26482927?v=4"
+          />
+          <div className={style.authorInfo}>
+            <strong>Guilherme Mendes</strong>
+            <span> Developer</span>
+          </div>
+        </div>
+        <time title="05 de janeiro de 2023" dataTime="2023-01-05 10:57:00">
+          Publicado à 1h.
+        </time>
+      </header>
+
+      <div className={style.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          <a href="#">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="#">#novoprojeto </a>
+          <a href="#">#nlw </a> <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={style.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Escreva um comentário..." />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={style.commentList}>
+        <Comments />
+      </div>
+    </article>
+  );
+}
